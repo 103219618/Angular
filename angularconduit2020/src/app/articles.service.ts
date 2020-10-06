bdd90cb1732560eaa560c6bfd16390d0932eba1e
Syed Razvi <103219618@swin.edu.au>
2020-10-06 20:04:00 +1100

    add ArticleService

---
+++ angularconduit2020/src/app/articles.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Article, ArticlesEnvelope } from './article';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArticlesService {
+
+  articles: Article[] = [];
+  loading: boolean = false;
+  loaded: boolean = false;
+
+  private httpClient: HttpClient;
+  constructor(httpClient: HttpClient){
+    this.httpClient = httpClient;
+  }
+/**
+ * go and get ArticlesEnvelope from the server ans store it locally
+ */
+  get() {
+    let request = this.httpClient.get<ArticlesEnvelope>("https://swindev.me/articles"); //https://conduit.productionready.io/api/articles
+    this.loading = true;
+    this.loaded = false;
+    request.subscribe((response) => {
+      this.articles = response.articles;
+      
+      this.loading = false;
+      this.loaded = true;
+    });
+  }
+}
