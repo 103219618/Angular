73341c0989e7442af2c2a92a69a2f2fb1f731d35
Syed Razvi <103219618@swin.edu.au>
2020-10-09 00:02:16 +1100

    initial routing build

---
+++ AngularRouting/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+
+  
+]; // sets up routes constant where you define your routes
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
